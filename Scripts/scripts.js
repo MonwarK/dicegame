014676207a567c1a891379248e7c9b30bded5ea2
Monwar Khan <72820069+MonwarK@users.noreply.github.com>
2020-10-22 18:34:27 +0100

    Add files via upload

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,12 +1,25 @@
+var dice1Value;
+var dice2Value;
+
 playerOneDice();
 playerTwoDice();
 
 function playerOneDice(){
-    var diceValue = Math.floor((Math.random() * 6) + 1);    
-    document.querySelector(".dice-one").setAttribute("src","images/dice"+diceValue+".png");
+    dice1Value = Math.floor((Math.random() * 6) + 1);    
+    document.querySelector(".dice-one").setAttribute("src","images/dice"+dice1Value+".png");
 }
 
 function playerTwoDice(){
-    var diceValue = Math.floor((Math.random() * 6) + 1);    
-    document.querySelector(".dice-two").setAttribute("src","images/dice"+diceValue+".png");
+    dice2Value = Math.floor((Math.random() * 6) + 1);    
+    document.querySelector(".dice-two").setAttribute("src","images/dice"+dice2Value+".png");
+}
+
+if(dice1Value > dice2Value){
+    document.querySelector("h1").innerHTML="Player One Wins!";
+}
+else if(dice1Value < dice2Value){
+    document.querySelector("h1").innerHTML="Player Two Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
 }
